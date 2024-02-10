a5a2bd1cd279cfecbb82ae8e0a334414a2ee0e15
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-02-10 16:34:30 +0100

    Added scripts component

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -191,8 +191,6 @@
 @endsection
 
 @section('scripts')
-
-<script  src="{{asset('assets/js/main.js')}}"></script>
-<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    <x-fixed.scripts></x-fixed.scripts>
 @endsection
 
