b6e2df5cb1d7f892fdc4982fe07dfaf12630452a
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-02-21 17:06:38 +0100

    Modified input component Added classInput prop

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -6,11 +6,12 @@
     'inputType' => '',
     'inputName' => '',
     'idInput' => '',
+    'classInput' => '',
     'idError' => '',
     'error' => '',
 ])
 
 
 <label for="{{ $for }}" id="{{ $idLabel }}" class="{{ $classLabel }}">{{ $label }}</label>
-<input type="{{ $inputType }}" name="{{$inputName}}" id="{{$idInput}}" >
+<input type="{{ $inputType }}" name="{{$inputName}}" id="{{$idInput}}" class="{{$classInput}}" >
 <p id="{{ $idError }}" class="error-message" style="display: none;">{{ $error }}</p>
