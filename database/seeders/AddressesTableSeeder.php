082a7c50e1428133e988f7cc5f73a57e3c9fbaf9
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-02-22 14:29:32 +0100

    Added Model, Migration and Seeder files for table roles To do: initialize the data that will be used in populating table

---
+++ database/seeders/AddressesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddressesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
