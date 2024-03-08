836b441071804740fe0a18f648724a1b912a334e
Nikola Samardžić <nikola.samardzic.it@gmail.com>
2024-03-08 17:08:16 +0100

    Added model, migration and seeder for comments and comment_image tables

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CommentRequest;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store(CommentRequest $request){
+
+    }
+}
